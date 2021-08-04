f777c9b596b3699127ffa5f7e777264cda2837d1
Breno0809 <bbreno08@gmail.com>
2021-08-04 17:45:57 -0300

    data display method done successfully

--- frontend/pages/patientArea/scripts/script.js
+++ frontend/pages/patientArea/scripts/script.js
@@ -1,8 +1,76 @@
 const url = 'http://localhost:8989/patients/'
+const patientInfo = document.querySelector('aside.userInfo')
+const nameSpace = document.querySelector('span.pName')
+
+const fieldNameAsArray = new Array(
+    'Código', 'Nome', 'Idade', 'Cartão do SUS', 'RG', 'CPF', 'Telefone', 'Celular',
+    'E-mail', 'responsável', 'E-mail do Responsável', 'Telefone do Responsável',
+    'Celular do Responsável', 'Endereço', 'Número', 'Bairro', 'Cidade', 'CEP',
+    'Comorbidade', 'Urgência')
+
 const getUser = async(endpoint, idParams) => {
-    const response = await fetch(`${endpoint}/user/${idParams}`)
-    const user = await response.json()
-    return user
+    try {
+        const response = await fetch(`${endpoint}user/${idParams}`)
+        const user = await response.json()
+        return user
+    } catch (error) {
+        console.error(error)
+        return false
+    }
 }
 
-console.log(getUser(url, 1))
+const calculateBirthday = date => {
+    const today = new Date(),
+        getBithday = new Date(date),
+        dateDiff = Math.abs(today.getTime() - getBithday.getTime())
+
+    return Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365))
+}
+
+const showPatientInfo = (data) => {
+    const user = data
+    nameSpace.innerHTML = `${user.nomeCompleto}`
+    for (const key in user) {
+        const keyAsName = key
+        if (Object.hasOwnProperty.call(user, keyAsName)) {
+            const value = user[keyAsName]
+            if (keyAsName == 'createdAt' || keyAsName == 'updatedAt') continue
+            else {
+                if (value == '') {
+                    patientInfo.innerHTML += `
+                    <div class="userColumn">
+                        <label>${keyAsName}</label>
+                        <div class="dataBox">
+                            <span class="empty">Campo não preenchido</span>
+                        </div>
+                    </div>`
+                } else {
+                    if (keyAsName == 'dataNascimento') {
+                        const idade = calculateBirthday(value)
+                        patientInfo.innerHTML += `
+                        <div class="userColumn">
+                            <label>${keyAsName}</label>
+                            <div class="dataBox">
+                                <span>${idade} anos</span>
+                            </div>
+                        </div>`
+                    } else {
+                        patientInfo.innerHTML += `
+                        <div class="userColumn">
+                            <label>${keyAsName}</label>
+                            <div class="dataBox">
+                                <span>${value}</span>
+                            </div>
+                        </div>`
+                    }
+                }
+            }
+        }
+    }
+}
+
+window.addEventListener('load', async() => {
+    const resp = await getUser(url, 8)
+    const user = resp.dataUser
+    showPatientInfo(user, fieldNameAsArray)
+})
