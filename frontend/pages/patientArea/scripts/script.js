a6cae03699ee482629f05421efaf9d2b5604e241
Breno0809 <bbreno08@gmail.com>
2021-08-03 23:57:28 -0300

    updated patient page

--- frontend/pages/patientArea/scripts/script.js
+++ frontend/pages/patientArea/scripts/script.js
@@ -1,1 +1,8 @@
-console.log('Welcome to the about page!');
+const url = 'http://localhost:8989/patients/'
+const getUser = async(endpoint, idParams) => {
+    const response = await fetch(`${endpoint}/user/${idParams}`)
+    const user = await response.json()
+    return user
+}
+
+console.log(getUser(url, 1))
