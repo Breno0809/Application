b475669c44e62fe7dbfc20f0a3372c3a9a5fe14c
Breno0809 <bbreno08@gmail.com>
2021-08-07 00:31:42 -0300

    first migration updated

--- api/src/database/migrations/20210807031232-create-patients-employee-historic.js
+++ api/src/database/migrations/20210807031232-create-patients-employee-historic.js
@@ -1,21 +1,26 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    up: async(queryInterface, Sequelize) => {
+        // return queryInterface.dropTable('user', {
+        //     id: {
+        //         type: Sequelize.INTEGER.ZEROFILL.UNSIGNED,
+        //         primaryKey: true,
+        //         autoIncrement: true,
+        //         allowNull: false
+        //     },
+        //     name: {
+        //       type: Sequelize.STRING,
+        //       allowNull: false,
+        //     },
+        //     email: {
+        //       type: Sequelize.STRING,
+        //       allowNull: false,
+        //     }
+        // })
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    down: async(queryInterface, Sequelize) => {
+        // return queryInterface.dropTable('user')
+    }
 };
