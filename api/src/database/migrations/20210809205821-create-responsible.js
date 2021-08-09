2502d0f68f1da0b15f844f056f1f7912c75d5d5d
Breno0809 <bbreno08@gmail.com>
2021-08-09 18:44:02 -0300

    migration of responsible model in progress. part.1

---
+++ api/src/database/migrations/20210809205821-create-responsible.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        return queryInterface.createTable('Responsible', {
+            idResponsible: {
+                type: Sequelize.INTEGER.ZEROFILL.UNSIGNED,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true
+            },
+            idContact: {
+                type: Sequelize.INTEGER.ZEROFILL.UNSIGNED,
+                allowNull: false,
+                reference: {
+                    model: 'Contact',
+                    key: 'idContact'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            nomeCompleto: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false
+            }
+        })
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Responsible')
+    }
+};
