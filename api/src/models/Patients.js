17b05e7ddf8da6a4ca56adc4b91c0ca52b6a9f74
Breno0809 <bbreno08@gmail.com>
2021-08-10 17:23:38 -0300

    patient model updated successfully

--- api/src/models/Patients.js
+++ api/src/models/Patients.js
@@ -9,25 +9,86 @@ const Patients = db.sequelize.define("Pacientes", {
         allowNull: false,
         primaryKey: true,
     },
-    nomeCompleto: { type: Sequelize.STRING, allowNull: false },
-    dataNascimento: { type: Sequelize.DATEONLY, allowNull: false },
-    cartaoSUS: { type: Sequelize.STRING(18), allowNull: false },
-    RG: { type: Sequelize.STRING(12), allowNull: false },
-    CPF: { type: Sequelize.STRING(14), allowNull: false },
-    telefone: { type: Sequelize.STRING(14), allowNull: false },
-    celular: { type: Sequelize.STRING(14), allowNull: false },
-    email: { type: Sequelize.STRING(100), allowNull: false },
-    responsavel: { type: Sequelize.STRING, allowNull: false },
-    emailResp: { type: Sequelize.STRING(100), allowNull: false },
-    telefoneResp: { type: Sequelize.STRING(14), allowNull: false },
-    celularResp: { type: Sequelize.STRING(14), allowNull: false },
-    endereco: { type: Sequelize.STRING, allowNull: false },
-    numero: { type: Sequelize.INTEGER, allowNull: false },
-    bairro: { type: Sequelize.STRING, allowNull: false },
-    cidade: { type: Sequelize.STRING, allowNull: false },
-    CEP: { type: Sequelize.STRING(10), allowNull: false },
-    comorbidade: { type: Sequelize.TEXT, allowNull: false },
-    urgencia: { type: Sequelize.STRING, allowNull: false }
+    idResponsible: {
+        type: Sequelize.INTEGER.ZEROFILL.UNSIGNED,
+        // allowNull: false,
+        references: {
+            model: 'Responsible',
+            key: 'idResponsible'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    },
+    idContact: {
+        type: Sequelize.INTEGER.ZEROFILL.UNSIGNED,
+        // allowNull: false,
+        references: {
+            model: 'Contact',
+            key: 'idContact'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    },
+    idSituation: {
+        type: Sequelize.INTEGER.ZEROFILL.UNSIGNED,
+        // allowNull: false,
+        references: {
+            model: 'Situation',
+            key: 'idSituation'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    },
+    idOxygen: {
+        type: Sequelize.INTEGER.ZEROFILL.UNSIGNED,
+        // allowNull: false,
+        references: {
+            model: 'Oxygen',
+            key: 'idOxygen'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    },
+    nomeCompleto: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    dataNascimento: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    cartaoSUS: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    RG: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    CPF: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    // telefone: { type: Sequelize.STRING, allowNull: false },
+    // celular: { type: Sequelize.STRING, allowNull: false },
+    // email: { type: Sequelize.STRING, allowNull: false },
+    // responsavel: { type: Sequelize.STRING, allowNull: false },
+    // emailResp: { type: Sequelize.STRING, allowNull: false },
+    // telefoneResp: { type: Sequelize.STRING, allowNull: false },
+    // celularResp: { type: Sequelize.STRING, allowNull: false },
+    // endereco: { type: Sequelize.STRING, allowNull: false },
+    // numero: { type: Sequelize.INTEGER, allowNull: false },
+    // bairro: { type: Sequelize.STRING, allowNull: false },
+    // cidade: { type: Sequelize.STRING, allowNull: false },
+    // CEP: { type: Sequelize.STRING, allowNull: false },
+    comorbidade: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    urgencia: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
 })
 
 // Create Table
