fef9e956d9432c5726bf3b2215ee82b7e5bd95ed
Breno0809 <bbreno08@gmail.com>
2021-08-26 14:14:09 -0300

    styling script created successfully

--- frontend/pages/sign-in/patient/script.js
+++ frontend/pages/sign-in/patient/script.js
@@ -1,23 +1,59 @@
-const inputSubmit = document.querySelector('input[type="submit"]')
+const buttonSubmit = document.querySelector('button[type="submit"]')
+const allInputs = document.querySelectorAll('input')
 
-inputSubmit.addEventListener('click', (event) => {
-    const dataForm = document.querySelectorAll('input[class="inputData"]')
+const addClassToField = (element, className = 'empty-class') => { element.classList.add(className) }
+const removeClassToField = (element, className = 'empty-class') => { element.classList.remove(className) }
 
-    // console.dir(event)
-
+buttonSubmit.addEventListener('click', event => {
+    const dataForm = document.querySelectorAll('input')
     dataForm.forEach(item => {
         const fieldValue = item.value,
-            itemAttributes = item.attributes,
-            itemAttributesName = itemAttributes[0].localName
-        console.log(fieldValue === '' ? 'Field empty' : fieldValue)
-            // console.dir(item)
-        console.log(itemAttributesName !== 'required' ?
-            'Dados do responsável' : // Aqui terão os dados do Responsável
-            'Dados do Paciente' // Aqui terão os dados do Paciente
-        );
+            itemTypeId = item.getAttribute('data-type-id'),
+            itemAttributeName = item.attributes[0].localName,
+            isRequired = itemAttributeName === 'required' ? true : false,
+            fieldIsEmpty = fieldValue === '' ? true : false
+
+        itemTypeId === 2 ? (fieldIsEmpty ? (
+            isRequired ? addClassToField(item, 'fieldEmpty') : undefined
+        ) : removeClassToField(item, 'fieldEmpty')) : (fieldIsEmpty ? (
+            isRequired ? addClassToField(item, 'fieldEmpty') : undefined
+        ) : removeClassToField(item, 'fieldEmpty'))
     })
 })
 
+allInputs.forEach(input => {
+    const inputDataset = Number(input.dataset.typeId)
+
+    const allAttributes = input.attributes
+    const attributesNameAsArray = []
+
+    for (let i = 0; i < allAttributes.length; i++) {
+        attributesNameAsArray.push(allAttributes[i].nodeName)
+    }
+    // console.log(attributesNameAsArray.find(attr => attr === 'required') === undefined ?
+    //     'não está aqui' :
+    //     'ENCONTRADO'
+    // )
+    // console.log(input);
+
+    input.addEventListener('blur', event => {
+            const inputValue = input.value
+            inputValue === '' ? (
+                // Caso campo esteja vazio
+                addClassToField(input, 'fieldEmpty'),
+                removeClassToField(input, 'fieldFilled')
+            ) : (
+                // Caso campo seja preenchido
+                addClassToField(input, 'fieldFilled'),
+                removeClassToField(input, 'fieldEmpty')
+            )
+        })
+        // if (inputDataset === 0) console.log('Dados do Paciente')
+        // if (inputDataset === 1) console.log('Endereço do Paciente')
+        // if (inputDataset === 2) console.log('Dados do Responsável')
+})
+
+// Não enviar formulário
 document.querySelector("form")
     .addEventListener("submit", event => {
         console.log("Enviando formulário!");
