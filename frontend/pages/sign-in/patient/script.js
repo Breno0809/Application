524fc6c45eafdf4361c62b9f391c171bf84c7780
Breno0809 <bbreno08@gmail.com>
2021-08-25 23:01:46 -0300

    Registration validation script in initiated

--- frontend/pages/sign-in/patient/script.js
+++ frontend/pages/sign-in/patient/script.js
@@ -1,1 +1,27 @@
-console.log("Welcome to the patient registration page");
+const inputSubmit = document.querySelector('input[type="submit"]')
+
+inputSubmit.addEventListener('click', (event) => {
+    const dataForm = document.querySelectorAll('input[class="inputData"]')
+
+    // console.dir(event)
+
+    dataForm.forEach(item => {
+        const fieldValue = item.value,
+            itemAttributes = item.attributes,
+            itemAttributesName = itemAttributes[0].localName
+        console.log(fieldValue === '' ? 'Field empty' : fieldValue)
+            // console.dir(item)
+        console.log(itemAttributesName !== 'required' ?
+            'Dados do responsável' : // Aqui terão os dados do Responsável
+            'Dados do Paciente' // Aqui terão os dados do Paciente
+        );
+    })
+})
+
+document.querySelector("form")
+    .addEventListener("submit", event => {
+        console.log("Enviando formulário!");
+
+        // Não envia o formulário
+        event.preventDefault()
+    })
