edecb0016c37655d9948f7a949352c1125b43ac8
Breno0809 <bbreno08@gmail.com>
2021-09-14 14:02:21 -0300

    main.cpp file updated

--- mobileDeviceConfig/src/main.cpp
+++ mobileDeviceConfig/src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
+#include <Wire.h>
+// #include "MAX30100_PulseOximeter.h"
 
 #define LED_BUILTIN 2
+#define reportingPeriodMs 1000
+// PulseOximeter pox;
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
